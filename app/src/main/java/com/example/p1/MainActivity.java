7be006c27c869cd89799a74f0043387c1c6817c1
LeeJueun <dlwndms2544@hanyang.ac.kr>
2021-06-30 20:53:40 +0900

    fragment switching

--- app/src/main/java/com/example/p1/MainActivity.java
+++ app/src/main/java/com/example/p1/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.p1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    Fragment frag_phone;
+    Fragment frag_gallery;
+    Fragment frag_free;
+    FragmentManager manager;
+    FragmentTransaction ft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        manager = getSupportFragmentManager();
+
+        Button bt_phone = findViewById(R.id.bt_phone);
+        Button bt_gallery = findViewById(R.id.bt_gallery);
+        Button bt_free = findViewById(R.id.bt_free);
+
+        frag_phone = new phone();
+        frag_gallery = new gallery();
+        frag_free = new free();
+
+        ft = manager.beginTransaction();
+        ft.add(R.id.fragment_container , frag_phone);
+        ft.addToBackStack(null);
+        ft.commit();
+
+        bt_phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,frag_phone).commit();
+            }
+        });
+        bt_gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container ,frag_gallery).commit();
+            }
+        });
+        bt_free.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,frag_free).commit();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/p1/PhoneAdapter.java
@@ -0,0 +1,43 @@
+//package com.example.p1;
+//
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.TextView;
+//
+//import androidx.annotation.NonNull;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//public class PhoneAdapter extends RecyclerView.Adapter<PhoneAdapter.ViewHolder> {
+//
+//    //private String[] localDataSet;
+//
+//    public static class ViewHolder extends RecyclerView.ViewHolder{
+//        private final TextView textView;
+//        public ViewHolder(View view){
+//            super(view);
+//            textView = (TextView) view.findViewById(R.id.tv_phone);
+//        }
+//        public TextView getTextView(){
+//            return textView;
+//        }
+//    }
+//
+//    public PhoneAdapter(String[] dataSet) {
+//        localDataSet = dataSet;
+//    }
+//
+//    public PhoneAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.)
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull PhoneAdapter.ViewHolder holder, int position) {
+//
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return 0;
+//    }
+//}
