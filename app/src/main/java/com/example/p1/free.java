f13d91158036024faf24ca02475686f0747f16cf
LeeJueun <dlwndms2544@hanyang.ac.kr>
2021-07-02 18:23:54 +0900

    세번째 탭 시작

--- app/src/main/java/com/example/p1/free.java
+++ app/src/main/java/com/example/p1/free.java
@@ -4,23 +4,51 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 
 public class free extends Fragment {
 
+    private EditText message;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_free, container, false);
+        View v = inflater.inflate(R.layout.fragment_free, container, false);
+        message = (EditText)v.findViewById(R.id.ti_input);
+
+        message.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                message.setText(s.toString());
+            }
+        });
+        return v;
     }
+
 }
